9070aa36c861bab8ab4d142c34960a87f8c53008
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-03-31 19:36:51 +0530

    ADDED:finding length of a line

--- Cartesian.java
+++ Cartesian.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Cartesian {
-	public static void main(String args[])
-	{
-		System.out.println("Welcome to Line Camparision Computaion Program");
-	}
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the value of x1 ");
+        int x1 = input.nextInt();
+
+        System.out.println("Enter the value of y1");
+        int y1 = input.nextInt();
+
+        System.out.println("Enter the value of x2 ");
+        int x2 = input.nextInt();
+
+        System.out.println("Enter the value of y2");
+        int y2 = input.nextInt();
+
+        double lengthOfLine = 0;
+        lengthOfLine = Math.sqrt(Math.pow((x2 - x1),2)+ Math.pow((y2 - y1),2));
+
+        System.out.println("Length of line is " + lengthOfLine);
+
+    }
 }
